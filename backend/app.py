0c9369f8b95c9632f39ff13068a243de157b528d
Aeklavya17 <aeklavya.rajgor@senkusha.in>
2024-07-13 16:12:46 +0530

    Initial Commit

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,7 @@ app.register_blueprint(event_bp)
 app.register_blueprint(carousel_bp)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
+    app.run(debug=True,host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
 
 # if __name__ == '__main__':
 #     app.run(debug=True,port=5001)
